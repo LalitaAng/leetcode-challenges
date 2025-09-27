1e821d06bc0696567a793bd5a2d33bcce9fa15fd
Lalita Angsuphanich <77669588+LalitaAng@users.noreply.github.com>
2025-09-27 17:48:21 +0200

    feat(problem1): add problem1 TwoSum solution with tests

---
+++ problem1/problem1.go
@@ -0,0 +1,17 @@
+package problem1
+
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order. */
+
+func TwoSum(nums []int, target int) []int {
+	indexNums := make(map[int]int)
+	for i, num := range nums {
+		if givenNum, ok := indexNums[target-num]; ok {
+			return []int{i, givenNum}
+		} else {
+			indexNums[num] = i
+		}
+	}
+	return nil
+}
